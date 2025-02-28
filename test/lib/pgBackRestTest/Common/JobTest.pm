57fa612fe92d205cf403957e14745aaeef15248c
David Steele <david@pgbackrest.org>
2025-02-28 15:38:05 -0600

    Fix multi-architecture unit testing.
    The Github action we were using for multi-architecture testing stopped working. The project does not seem to be getting regular maintenance so it seems better to roll multi-architecture testing into our existing container builds.
    
    Introduce multi-architecture builds and testing into our test framework. For now this only works for unit tests -- integration tests will still only run on x86_64. That could be updated in the future but since emulation is so slow it is not clear if it would be useful.
    
    Also fix an invalid 32-bit checksum. The d11 test had not been running as 32-bit since d8ff89a so the checksum was not updated when it should have been in 48f511d.
    
    (cherry picked from commit 57ffd2dfcbc47300d61d901789c6d09ffc8e25b2)

--- test/lib/pgBackRestTest/Common/JobTest.pm
+++ test/lib/pgBackRestTest/Common/JobTest.pm
@@ -52,6 +52,8 @@ sub new
         $self->{oTest},
         $self->{bDryRun},
         $self->{bVmOut},
+        $self->{strPlatform},
+        $self->{strImage},
         $self->{iVmIdx},
         $self->{iVmMax},
         $self->{strMakeCmd},
@@ -85,6 +87,8 @@ sub new
             {name => 'oTest'},
             {name => 'bDryRun'},
             {name => 'bVmOut'},
+            {name => 'strPlatform'},
+            {name => 'strImage'},
             {name => 'iVmIdx'},
             {name => 'iVmMax'},
             {name => 'strMakeCmd'},
@@ -208,15 +212,15 @@ sub run
                     my $strBuildPath = $self->{strTestPath} . '/build/' . $self->{oTest}->{&TEST_VM};
 
                     executeTest(
-                        'docker run -itd -h ' . $self->{oTest}->{&TEST_VM} . "-test --name=${strImage}" .
-                        " -v ${strHostTestPath}:${strVmTestPath}" .
+                        'docker run' . $self->{strPlatform} . ' -itd -h ' .
+                        $self->{oTest}->{&TEST_VM} . "-test --name=${strImage} -v ${strHostTestPath}:${strVmTestPath}" .
                         ($self->{oTest}->{&TEST_C} ? " -v $self->{strUnitPath}:$self->{strUnitPath}" : '') .
                         ($self->{oTest}->{&TEST_C} ? " -v $self->{strDataPath}:$self->{strDataPath}" : '') .
                         " -v $self->{strBackRestBase}:$self->{strBackRestBase}" .
                         " -v $self->{strRepoPath}:$self->{strRepoPath}" .
                         ($self->{oTest}->{&TEST_C} ? " -v ${strBuildPath}:${strBuildPath}:ro" : '') .
                         ($self->{oTest}->{&TEST_C} ? " -v ${strCCachePath}:/home/${\TEST_USER}/.ccache" : '') .
-                        ' ' . containerRepo() . ':' . $self->{oTest}->{&TEST_VM} . '-test',
+                        ' ' . $self->{strImage},
                         {bSuppressStdErr => true});
                 }
             }
